45f6a43f2d0133338978b8c90e967ef336362aa0
Jackie Murphy <john.murphy@rallyhealth.com>
2020-07-30 13:58:43 -0400

    Actually add tests

---
+++ src/test/java/sample/SomeTest.java
@@ -0,0 +1,18 @@
+package sample;
+
+import static org.junit.Assert.*;
+public class SomeTest {
+    @org.junit.Test
+    public void checks() {}
+
+    @org.junit.Test
+    public void checks2() {}
+
+    @org.junit.Test
+    public void checks3() {}
+
+    @org.junit.Test
+    public void checks4() {
+        assertEquals(42,43);
+    }
+}
